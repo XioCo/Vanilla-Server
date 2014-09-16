a1177918c3315fcc97c5103c05ef2a97965bb020
jimbo8 <t.dale98@gmail.com>
2014-09-16 19:20:34 +0200

    Forgot permissions for open-inv.

--- XioCo Plugin/src/no/xioco/commands/OpenInvCommand.java
+++ XioCo Plugin/src/no/xioco/commands/OpenInvCommand.java
@@ -14,42 +14,46 @@ import org.bukkit.inventory.Inventory;
 public class OpenInvCommand implements CommandExecutor{
     public boolean onCommand(CommandSender sender, Command cmd, String cmdl, String[] args) {
         if(cmd.getName().equalsIgnoreCase("open")){
-            if(sender instanceof Player){
+            if(sender instanceof Player) {
                 Player player = (Player) sender;
-                if(args.length == 2){
-                    if(args[0].equalsIgnoreCase("inv")) {
-                        //TEMPORARY SOLUTION! For some reason, getting the player returns null even when it is checked for. Will figure it out later.
-                        for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
-                            if (onlinePlayers.getName().equalsIgnoreCase(args[1])) {
-                                if (!args[1].equalsIgnoreCase(player.getName())) {
-                                    Inventory inv = onlinePlayers.getInventory();
-                                    player.openInventory(inv);
+                if (player.hasPermission("xioco.openinv")) {
+                    if (args.length == 2) {
+                        if (args[0].equalsIgnoreCase("inv")) {
+                            //TEMPORARY SOLUTION! For some reason, getting the player returns null even when it is checked for. Will figure it out later.
+                            for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
+                                if (onlinePlayers.getName().equalsIgnoreCase(args[1])) {
+                                    if (!args[1].equalsIgnoreCase(player.getName())) {
+                                        Inventory inv = onlinePlayers.getInventory();
+                                        player.openInventory(inv);
+                                    } else {
+                                        player.sendMessage(ChatColor.RED + "Fy på deg! Hvorfor prøver du å åpne din egen inventory? :(");
+                                    }
                                 } else {
-                                    player.sendMessage(ChatColor.RED + "Fy på deg! Hvorfor prøver du å åpne din egen inventory? :(");
+                                    player.sendMessage(ChatColor.RED + "Brukeren: " + ChatColor.WHITE + args[1] + ChatColor.RED + " er ikke online!");
                                 }
-                            } else {
-                                player.sendMessage(ChatColor.RED + "Brukeren: " + ChatColor.WHITE + args[1] + ChatColor.RED + " er ikke online!");
                             }
-                        }
-                    }else if(args[0].equalsIgnoreCase("enderchest")) {
-                        //TEMPORARY SOLUTION! For some reason, getting the player returns null even when it is checked for. Will figure it out later.
-                        for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
-                            if (onlinePlayers.getName().equalsIgnoreCase(args[1])) {
-                                if (!args[1].equalsIgnoreCase(player.getName())) {
-                                    Inventory inv = onlinePlayers.getEnderChest();
-                                    player.openInventory(inv);
+                        } else if (args[0].equalsIgnoreCase("enderchest")) {
+                            //TEMPORARY SOLUTION! For some reason, getting the player returns null even when it is checked for. Will figure it out later.
+                            for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
+                                if (onlinePlayers.getName().equalsIgnoreCase(args[1])) {
+                                    if (!args[1].equalsIgnoreCase(player.getName())) {
+                                        Inventory inv = onlinePlayers.getEnderChest();
+                                        player.openInventory(inv);
+                                    } else {
+                                        player.sendMessage(ChatColor.RED + "Fy på deg! Hvorfor prøver du å åpne din egen inventory? :(");
+                                    }
                                 } else {
-                                    player.sendMessage(ChatColor.RED + "Fy på deg! Hvorfor prøver du å åpne din egen inventory? :(");
+                                    player.sendMessage(ChatColor.RED + "Brukeren: " + ChatColor.WHITE + args[1] + ChatColor.RED + " er ikke online!");
                                 }
-                            } else {
-                                player.sendMessage(ChatColor.RED + "Brukeren: " + ChatColor.WHITE + args[1] + ChatColor.RED + " er ikke online!");
                             }
-                        }
-                        }else{
+                        } else {
                             player.sendMessage(ChatColor.RED + "Denne subkommandoen finnes ikke. Bruk /open <inv/enderchest> [brukernavn]");
                         }
+                    }
+                } else {
+                    player.sendMessage(ChatColor.RED + "Du har ikke tilgang til denne kommandoen.");
                 }
-            }else{
+            } else {
                 sender.sendMessage("Du ma vere ingame for a bruke denne kommandoen!");
             }
             return true;
